e6c91f85ad56af2169580726e30b46ddaa4bdbd8
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-04-07 10:07:58 +0800

    sync 2025/04/07

--- algorithm/src/main/kotlin/com/only4/algorithm/leetcode/reverseList.kt
+++ algorithm/src/main/kotlin/com/only4/algorithm/leetcode/reverseList.kt
@@ -2,8 +2,8 @@ package com.only4.algorithm.leetcode
 
 fun reverseList(head: ListNode?): ListNode? {
     tailrec fun dp (prev: ListNode?, curr: ListNode?): ListNode? {
-        return when {
-            curr == null -> prev
+        return when(curr) {
+            null -> prev
             else -> dp(curr, curr.next.also { curr.next = prev })
         }
     }
