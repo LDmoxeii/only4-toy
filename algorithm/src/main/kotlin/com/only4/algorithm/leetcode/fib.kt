3cb8756bbdfdfbe0c5c0bd94d58786cd70ec91ab
LD_moxeii <15976781430@163.com>
2025-07-04 20:32:27 +0800

    迁移算法模块

--- algorithm/src/main/kotlin/com/only4/algorithm/leetcode/fib.kt
+++
@@ -1,9 +0,0 @@
-package com.only4.algorithm.leetcode
-
-fun fib(n: Int): Int {
-    val memo = IntArray(n + 1) { 0 }
-    if (n == 0) return 0
-    if (n == 1) return 1
-    if (memo[n] != 0) return memo[n]
-    return fib(n - 2) + fib(n - 1)
-}
